c41354d526ec54f2999b948938cdaadd6c3ceaa4
志賀大河 <shigataiga@shigataiganoMacBook-Pro.local>
2020-01-11 19:49:54 +0900

    First Commit

---
+++ LoginFacebookApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginFacebookApp
+//
+//  Created by 志賀大河 on 2020/01/11.
+//  Copyright © 2020 Taigashiga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
